5ef195264939becc413c657176331f14754208d9
dhelbert <dhelbert@deloitte.com>
2025-06-03 09:19:58 -0700

    Added anonymous class

--- src/main/java/org/example/concurrent/Threads.java
+++ src/main/java/org/example/concurrent/Threads.java
@@ -6,11 +6,20 @@ public class Threads {
         Thread t2 = new MyThread();
         Thread t3 = new Thread(() -> {System.out.println("Runnable Lambda");});
 
+        // Anonymous class
+        Runner runner = new Runner() {
+                @Override
+                public void run() {System.out.println("Anonymous Runnable");}
+        };
+
+        Thread t4 = new Thread(runner);
+
         t1.setDaemon(true); // JVM will terminate if only daemon threads are running
 
         t1.start();
         t2.start();
         t3.start();
+        t4.start();
 
         try {
             System.out.println("Waiting for t3 to complete...");
